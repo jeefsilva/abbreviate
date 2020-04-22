39317faab57d5ed9060c79b365d3e41f65b909dd
Jefferson Silva <jbds.web@gmail.com>
2020-04-22 13:04:25 -0300

    Convertendo para javascript puro

--- abbreviate.js
+++ abbreviate.js
@@ -1,22 +1,21 @@
 //Abreviação por ordem de 2 em 2 decimais
 const abbrCode = ["", "K", "M", "B", "T"];
 
-(function($) {
-  $.fn.abbreviate = function(abbrName = true, decimals = 2) {
-    for (let index = 0; index < $(this).length; index++) {
-      let count = $(this)[index].textContent;
-      //Verifica a posição da abreviação, se é necessário abreviar e inserir o tipo de abreviação
-      const i = 0 === count ? count : Math.floor(Math.log(count) / Math.log(1000));
-      //Converte o número em decimal e formata em ponto fixo de acordo com a variável decimals
-      let result = parseFloat((count / Math.pow(1000, i)).toFixed(decimals));
-      //Se a variável abbrName estiver como verdadeiro é inserido o tipo de abreviação
-      if (abbrName) {
-        result += `${abbrCode[i]}`;
-      }
-      $(this)[index].textContent = result;
+function abbreviate(abbrName = true, decimals = 2) {
+  for (let index = 0; index < abbr.length; index++) {
+    let count = abbr[index].textContent;
+    //Verifica a posição da abreviação, se é necessário abreviar e inserir o tipo de abreviação
+    const i = 0 === count ? count : Math.floor(Math.log(count) / Math.log(1000));
+    //Converte o número em decimal e formata em ponto fixo de acordo com a variável decimals
+    let result = parseFloat((count / Math.pow(1000, i)).toFixed(decimals));
+    //Se a variável abbrName estiver como verdadeiro é inserido o tipo de abreviação
+    if (abbrName) {
+      result += `${abbrCode[i]}`;
     }
-  };
-})(jQuery);
+    abbr[index].textContent = result;
+  }
+}
 
 //Chamando a função através da classe .abbreviate
-$(".abbreviate").abbreviate(true, 4);
+var abbr = document.querySelectorAll(".abbreviate");
+abbreviate(true, 4, abbr);
